875141cbbb82bf2eb8cd52972540af0fa591a0ce
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-11-08 10:00:11 -0800

    added activate

---
+++ k8s/activate-kubeconfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+MY_DIR="$(dirname $MY_PATH)"
+
+source ${MY_DIR}/configuration.sh
+
+echo "export KUBECONFIG=${K8S_CONFIG_PATH}"
