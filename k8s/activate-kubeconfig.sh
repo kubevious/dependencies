ce9e158b32c017d626b13cd77d540ef590ac4541
Ruben Hakopian <ruben.hakopian@gmail.com>
2023-10-22 19:55:15 -0700

    fixed dependencies

--- k8s/activate-kubeconfig.sh
+++ k8s/activate-kubeconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
-MY_DIR="$(dirname $MY_PATH)"
+DEPS_K8S_MY_DIR="$(dirname $MY_PATH)"
 
-source ${MY_DIR}/configuration.sh
+source ${DEPS_K8S_MY_DIR}/configuration.sh
 
 echo "export KUBECONFIG=${K8S_CONFIG_PATH}"
--- k8s/configuration.sh
+++ k8s/configuration.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
-MY_DIR="$(dirname $MY_PATH)"
+DEPS_K8S_MY_DIR="$(dirname $MY_PATH)"
 
 export K8S_CLUSTER_NAME=kubevious-oss-dev
-export K8S_CONFIG_PATH=${MY_DIR}/runtime/kube-config.yaml
+export KUBECONFIG=${DEPS_K8S_MY_DIR}/runtime/kube-config.yaml
+export K8S_CONFIG_PATH=${KUBECONFIG}
 
-export CA_CERTS_FOLDER=${MY_DIR}/runtime/certs
+export CA_CERTS_FOLDER=${DEPS_K8S_MY_DIR}/runtime/certs
